32a7feb16a701176cd3f5e1e07deb2ee9ac51fd8
Antonio Macrì <ing.antonio.macri@gmail.com>
2014-01-08 00:52:20 +0100

    [ssoa-registry] Small changes to documentation.

--- ssoa-registry/include/registrylistener.h
+++ ssoa-registry/include/registrylistener.h
@@ -14,40 +14,37 @@
 
 namespace ssoa
 {
-    /// The top-level class of the registry server.
+    /// Represents the listening registry server.
     class RegistryListener: private boost::noncopyable
     {
     public:
-        /// Initializes an instance of the server, binding it to the specified TCP address and port,
-        /// and starts listening for incoming connections.
+        /// Initializes an instance of the server and starts listening for incoming connections.
         ///
-        /// @param host A string identifying a host. May be a descriptive name or a numeric address
-        /// string. If empty, the loopback address will be used.
-        ///
+        /// @param host A string identifying a host. May be a descriptive name or a numeric
+        ///        address string. If empty, the loopback address will be used.
         /// @param port A string containing the port number.
+        /// @param thread_pool_size The number of threads allocated in the pool.
         ///
         /// @throws boost::system::system_error Thrown on failure.
         explicit RegistryListener(const std::string& host, const std::string& port, std::size_t thread_pool_size);
 
-        /// Run the server's io_service loop.
+        /// Runs the server's io_service loop.
         void run();
 
     private:
-        /// Initiate an asynchronous accept operation.
+        /// Initiates an asynchronous accept operation.
         void startAccept();
 
-        /// Handle completion of an asynchronous accept operation.
+        /// Handles completion of an asynchronous accept operation.
         void handleAccept(const boost::system::error_code& e);
 
-        /// Handle a request to stop the server.
+        /// Handles a request to stop the server.
         void handleStop();
 
         /// The number of threads that will call io_service::run().
         std::size_t threadPoolSize;
 
         /// The io_service used to perform asynchronous operations.
-        /// The io_service class it deals with the operating system, waiting for all asynchronous
-        /// operations to end, and then calling the completion handler for each such operation.
         boost::asio::io_service ioService;
 
         /// The signal_set is used to register for process termination notifications.
@@ -57,7 +54,6 @@ namespace ssoa
         boost::asio::ip::tcp::acceptor acceptor;
 
         /// The next connection to be accepted.
-
         std::shared_ptr<ClientHandler> clientHandler;
     };
 }
